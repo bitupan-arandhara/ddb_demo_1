8228dd11de405fb214b7ea472fca032567576d83
bitupan-arandhara <bitupan.arandhara@swiggy.in>
2022-09-07 13:37:20 +0530

    //

--- src/main/java/com/bitupan/Transaction.java
+++ src/main/java/com/bitupan/Transaction.java
@@ -8,7 +8,6 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverted;
-//import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBVersionAttribute;
 import com.bitupan.models.Customer;
 import com.bitupan.translators.CustomerTranslatior;
 
